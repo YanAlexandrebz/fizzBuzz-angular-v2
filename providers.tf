fbeb428f49a6b2220f11bd9453480ab5c386c275
Yan Alexandre <rm77740@fiap.com.br>
2025-03-11 19:41:11 -0300

    fix terraform files

--- providers.tf
+++ providers.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "=> 3.0.0"
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
     }
   }
 
   backend "s3" {
     bucket = "rm354938bucketfiap"
-    key = "dev.tfstate"
+    key    = "dev.tfstate"
     region = "us-east-1"
   }
 }
